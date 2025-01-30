ab498404a964070214f1a763f8940b3ed24b0372
Aditya Ardak <58509583+adityaardak@users.noreply.github.com>
2025-01-30 12:37:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 import streamlit as st
+import pandas as pd
 
 st.header("This is a header")
 st.subheader("This is a subheader")
+
+data = {
+  "Fruit": ["Apple", "Banana", "Cherry"],
+  "Price": [12, 8, 2.5],
+  "Quantity": [1, 2, 1]
+}
+
+df = pd.DataFrame(data)
+st.dataframe(df)
+
