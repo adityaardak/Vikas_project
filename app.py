1a4ead0a7a67f22b7981ae2865c86ddebad824eb
Aditya Ardak <58509583+adityaardak@users.noreply.github.com>
2025-01-30 12:49:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
-slider_value = st.slider("Select a value using the slider: ", min_value = 0, max_value = 50, value = 50)
-st.write(f"You have selected {slider_value}")
+slider_value_1 = st.slider("Select a value using the slider: ", min_value = 0, max_value = 50, value = 50)
+slider_value_2 = st.slider("Select a value using the slider: ", min_value = 0, max_value = 50, value = 50)
+st.write(f"You have selected {slider_value_1 + slider_value_2}")
