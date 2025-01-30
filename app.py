02a595f8ff1ee0d9d268f59a5e0e65f8cd6a9116
Aditya Ardak <58509583+adityaardak@users.noreply.github.com>
2025-01-30 12:46:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
 
-user_age = st.number_input("Enter your age: ", min_value = 0, max_value = 60, value = 30)
-
-st.write(f"Your age is {user_age}")
+if st.button("Click Me!"):
+  st.write("You have clicked on the button.")
