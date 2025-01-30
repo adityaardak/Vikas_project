e419b457ff87879fab95409d37b60ba375d2454b
Aditya Ardak <58509583+adityaardak@users.noreply.github.com>
2025-01-30 12:54:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
-slider_value_1 = int(st.slider("Select a value using the slider: ", min_value = 0, max_value = 50, value = 50))
-st.write(f"Summation:  {slider_value_1}")
+fav_color = st.radio("pick any color", option = ["red", "blue", "green"])
+
+st.write(f"You have selected {fav_color} color")
