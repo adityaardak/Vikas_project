5c50a1d9eb870bb98bf9c63c6fb098d7b6ad74d8
Aditya Ardak <58509583+adityaardak@users.noreply.github.com>
2025-01-30 12:55:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
-fav_color = st.radio("pick any color", option = ["red", "blue", "green"])
+color_list = ["red", "blue", "green"]
+
+fav_color = st.radio("pick any color", option = color_list)
 
 st.write(f"You have selected {fav_color} color")
